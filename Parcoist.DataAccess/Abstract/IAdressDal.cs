139cd0c3b5ddf1f5bb910e9e0b87d62e40f91748
Yusuf Keles <kkelesyusuf23@gmail.com>
2025-06-13 22:41:28 +0300

    dataaccess abstract tamamlandı. identity yok

---
+++ Parcoist.DataAccess/Abstract/IAdressDal.cs
@@ -0,0 +1,13 @@
+﻿using Parcoist.UI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcoist.DataAccess.Abstract
+{
+    public interface IAdressDal:IGenericDal<Adress>
+    {
+    }
+}
