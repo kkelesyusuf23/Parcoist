d4986d5e9eb4531ed387c6208a23a1f532527b53
Yusuf Keles <kkelesyusuf23@gmail.com>
2025-06-29 21:59:43 +0300

    ...

---
+++ Parcoist.DataAccess/Migrations/20250629185928_mig_product_add_column_links.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcoist.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_product_add_column_links : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Link1",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link2",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Link3",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link1",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Link2",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Link3",
+                table: "Products");
+        }
+    }
+}
--- Parcoist.Entity/Concrete/Product.cs
+++ Parcoist.Entity/Concrete/Product.cs
@@ -16,6 +16,10 @@
         public int Stock { get; set; }
         public string Description { get; set; }
 
+        public string? Link1 { get; set; }
+        public string? Link2 { get; set; }
+        public string? Link3 { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public DateTime UptatedAt { get; set; }
 
